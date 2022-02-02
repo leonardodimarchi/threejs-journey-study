1715d58de75f9acd84064ed7a5a2a709bb94e7b7
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-02-01 21:35:14 -0300

    adding(09-geometries): adding geometries lesson

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { _SRGBAFormat } from 'three'
 
 // Cursor
