b112b6a3fec057add62c43e77443be94cef64e7e
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-02-18 21:19:11 -0300

    update(23-imported-models): adding fox model with animations

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -1,6 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/dracoloader'
 import * as dat from 'lil-gui'
 
 /**
@@ -14,6 +16,31 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null;
+
+gltfLoader.load(
+    '/models/Fox/glTF/Fox.gltf',
+    (model) => {
+        model.scene.scale.set(0.025, 0.025, 0.025)
+        scene.add(model.scene)
+
+        mixer = new THREE.AnimationMixer(model.scene)
+        const action = mixer.clipAction(model.animations[2])
+
+        action.play()
+    }
+)
+
 
 /**
  * Floor
@@ -105,6 +132,10 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    // Update mixer
+    if (mixer !== null) 
+        mixer.update(deltaTime)
 
     // Update controls
     controls.update()
